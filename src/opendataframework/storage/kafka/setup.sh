fb68003667d00ea6cda2e0793d86ae4b882e896f
mykytapavlov <mykytapavlov@users.noreply.github.com>
2024-12-19 23:39:47 +0200

    :ambulance: fix(storage.kafka): setup bootstrap server port

--- src/opendataframework/storage/kafka/setup.sh
+++ src/opendataframework/storage/kafka/setup.sh
@@ -1,2 +1,2 @@
 echo ">> Setting up Kafka"
-docker exec -it project_name_kafka bash -c '/opt/kafka/bin/kafka-topics.sh --bootstrap-server project_name-kafka --create --topic entities'
+docker exec -it project_name_kafka bash -c '/opt/kafka/bin/kafka-topics.sh --bootstrap-server project_name-kafka:9092 --create --topic entities'
